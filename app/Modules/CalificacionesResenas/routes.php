ffd2fd15251783afde6cab0aa7697b8cb195e6ff
Jeffer <yapupiales42@soy.sena.edu.com>
2025-11-25 17:02:19 -0500

    Se renombran las rutas internas de los módulos.

--- app/Modules/CalificacionesResenas/routes.php
+++ app/Modules/CalificacionesResenas/routes.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Modules\CalificacionesResenas\Controllers\ResenasController;
 
-Route::prefix('calificaciones-resenas')->name('calificaciones-resenas')->group(function () {
+Route::prefix('calificaciones-resenas')->name('calificaciones.resenas')->group(function () {
     Route::get('/', [ResenasController::class, 'index'])->name('index');
 });
