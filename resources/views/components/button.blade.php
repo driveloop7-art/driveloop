bb19bbea4da41c67f832626c96bb4bf354ef9d0c
Jeffer <yapupiales42@soy.sena.edu.com>
2025-12-13 18:52:22 -0500

    Add new components

---
+++ resources/views/components/button.blade.php
@@ -0,0 +1,37 @@
+@props([
+    'width' => 'auto',
+    'gradient' => false,
+])
+
+@php
+    $width = [
+        'auto' => 'w-auto',
+        'sm' => 'w-[5%]',
+        'md' => 'w-[15%]',
+        'lg' => 'w-[30%]',
+        'xl' => 'w-[40%]',
+        '2xl' => 'w-[50%]',
+        'full' => 'w-full',
+    ][$width];
+
+    if ($gradient) {
+        $classes = 'bg-gradient-to-r from-dl to-dl-two hover:from-dl-two hover:to-dl-two';
+    } else {
+        $classes = 'bg-dl hover:bg-dl-two';
+    }
+@endphp
+
+<button {{ $attributes->merge([
+    'type' => 'submit',
+    'class' => 'inline-flex
+        xl:rounded-md
+        justify-center
+        px-14 py-3
+        tracking-widest
+        border border-transparent
+        font-semibold text-xs text-white uppercase
+        active:bg-dl-two focus:outline-none focus:ring-1 focus:ring-dl-three
+        transition ease-in-out duration-150 ' . $width . ' ' . $classes]) }}>
+        
+    {{ $slot }}
+</button>
---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,3 @@
+<div class="xl:rounded-xl p-8 shadow-xl ring-1 ring-dl/30 text-center">
+    {{ $slot }}
+</div>
