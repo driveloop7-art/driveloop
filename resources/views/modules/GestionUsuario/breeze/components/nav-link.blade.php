1caab480fbfeb9ade5d70712017f36d9a2b23115
Jeffer <yapupiales42@soy.sena.edu.com>
2025-12-13 18:56:04 -0500

    Update colors components breeze

--- resources/views/modules/GestionUsuario/breeze/components/nav-link.blade.php
+++ resources/views/modules/GestionUsuario/breeze/components/nav-link.blade.php
@@ -2,8 +2,17 @@
 
 @php
 $classes = ($active ?? false)
-            ? 'inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
-            : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out';
+            ? 'inline-flex items-center px-1 pt-1 border-b-2 border-dl
+            text-sm text-dl font-medium leading-5
+            focus:outline-none focus:border-dl-three focus:text-dl-three
+            hover:text-dl-three hover:border-dl-three
+            transition duration-150 ease-in-out'
+            :
+            'inline-flex items-center px-1 pt-1 border-b-2 border-transparent
+            text-sm font-medium leading-5 text-gray-500
+            hover:text-dl-three hover:border-dl-three
+            focus:outline-none focus:text-dl-three focus:border-dl-three
+            transition duration-150 ease-in-out';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
