618999bee799eaab3643ba1b6a62909b4b795c77
Jeffer <yapupiales42@soy.sena.edu.com>
2025-12-16 17:57:14 -0500

    Update page Dashboard

--- resources/views/modules/GestionUsuario/breeze/dashboard.blade.php
+++ resources/views/modules/GestionUsuario/breeze/dashboard.blade.php
@@ -1,8 +1,8 @@
-<x-app-layout>
+<x-page>
     
     {{ __("You're logged in!") }}
     
-</x-app-layout>
+</x-page>
 
 <!-- <x-slot name="header">
     <h2 class="font-semibold text-xl text-gray-800 leading-tight">
