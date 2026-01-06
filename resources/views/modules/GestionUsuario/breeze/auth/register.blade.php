f7305ee6377ea204761d4b57dfedccdf1c091f76
Nicolas1056 <nicolassalazar2k19@gmail.com>
2026-01-05 21:10:51 -0500

    CheckBox terminos y condiciones

--- resources/views/modules/GestionUsuario/breeze/auth/register.blade.php
+++ resources/views/modules/GestionUsuario/breeze/auth/register.blade.php
@@ -49,6 +49,19 @@
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
+        <!-- Terms and Conditions -->
+        <div class="mt-4">
+            <label for="terms" class="inline-flex items-center">
+                <input id="terms" type="checkbox"
+                    class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="terms"
+                    required>
+                <span class="ms-2 text-sm text-gray-600">{{ __('Acepto') }} <a href="{{ asset('terminos.pdf') }}" target="_blank"
+                        class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        >{{ __('Terminos y Condiciones') }}</a></span>
+            </label>
+            <x-input-error :messages="$errors->get('terms')" class="mt-2" />
+        </div>
+
         <div class="flex items-center justify-end mt-4">
             <x-button width="full" x-data="">{{ __('Join us') }}</x-button>
         </div>
@@ -59,4 +72,5 @@
             </a>
         </div>
     </form>
+
 </x-split-layout>
