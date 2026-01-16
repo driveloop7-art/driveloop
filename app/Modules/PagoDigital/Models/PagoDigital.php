07813d26a3dbc71bd1aadca668ecc38cc1d9c48b
Nicolas1056 <nicolassalazar2k19@gmail.com>
2026-01-15 20:25:33 -0500

    Inicio Modulo de PagoDigital

---
+++ app/Modules/PagoDigital/Models/PagoDigital.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Modules\PagoDigital\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PagoDigital extends Model
+{
+    //
+}
--- app/Modules/PagoDigital/routes.php
+++ app/Modules/PagoDigital/routes.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Modules\PagoDigital\Controllers\PagoDigitalController;
 
 Route::prefix('pago-digital')->group(function () {
     Route::get('/', function() { return view("modules.PagoDigital.index"); })->name('pago.digital');
