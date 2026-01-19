48b09264dc72e4276ce95c0cbaae8bcc2ddd6594
Sebastian <sebasti.m.0310@gmail.com>
2026-01-18 19:38:02 -0500

    Fix rutas de gestion usuario

--- app/Modules/GestionUsuario/routes.php
+++ app/Modules/GestionUsuario/routes.php
@@ -3,9 +3,10 @@ use App\Modules\GestionUsuario\Controllers\DocumentoUsuarioController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('gestion-usuario')->group(function () {
-<<<<<<< HEAD
     Route::get('/', function () {
-        return view("modules.GestionUsuario.index"); })->name('gestion.usuario');
+        return view("modules.GestionUsuario.index");
+    })->name('gestion.usuario');
+    Route::get('/my-trips', fn() => view('modules.GestionUsuario.breeze.my-trips'))->name('my-trips');
     // Grupo de rutas que requieren autenticación
     Route::middleware(['auth'])->group(function () {
         // Ruta para ver la gestión de documentos
@@ -16,8 +17,3 @@ Route::prefix('gestion-usuario')->group(function () {
             ->name('usuario.documentos.store');
     });
 });
-=======
-    Route::get('/', function() { return view("modules.GestionUsuario.index"); })->name('gestion.usuario');
-    Route::get('/my-trips', fn() => view('modules.GestionUsuario.breeze.my-trips'))->name('my-trips');
-});
->>>>>>> 3aaeeabc2300d76d55d1fef4aef03338a06cf603
