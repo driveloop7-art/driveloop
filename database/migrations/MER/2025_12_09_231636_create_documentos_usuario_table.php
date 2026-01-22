938513580dcd959d98521f47fe631ff5e968316e
Sebastian <sebasti.m.0310@gmail.com>
2026-01-22 13:23:18 -0500

    Modificacion de migracion documentos_usuario

--- database/migrations/MER/2025_12_09_231636_create_documentos_usuario_table.php
+++ database/migrations/MER/2025_12_09_231636_create_documentos_usuario_table.php
@@ -15,6 +15,22 @@ return new class extends Migration {
             $table->integer('idtipdocusu')->index();
             $table->string('num', 45);
             $table->unsignedBigInteger('codusu')->nullable()->index();
+            //Nuevos campos
+            // Ruta relativa donde se almacenará el anverso 
+            // Se usa string 255 es suficiente para rutas relativas.
+            $table->string('url_anverso', 255)->nullable()->after('num');
+            // Ruta relativa donde se almacenará el reverso 
+            $table->string('url_reverso', 255)->nullable()->after('url_anverso');
+
+            // Estado de la verificación. Por defecto es PENDIENTE al subirlo.
+            $table->enum('estado', ['PENDIENTE', 'APROBADO', 'RECHAZADO'])
+                ->default('PENDIENTE');
+
+            // Mensaje opcional para explicar por qué se rechazó (si aplica)
+            $table->text('mensaje_rechazo')->nullable();
+
+            // Relación con la tabla users
+            $table->foreign(['codusu'], 'docsusuario_users_fk')->references(['id'])->on('users')->onUpdate('cascade')->onDelete('no action');
         });
     }
 
