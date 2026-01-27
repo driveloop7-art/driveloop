f8e74a3560af7e4907a27ffc6f487db013eee436
Sebastian <sebasti.m.0310@gmail.com>
2026-01-26 21:38:34 -0500

    vite

--- vite.config.js
+++ vite.config.js
@@ -13,11 +13,7 @@ export default defineConfig({
     },
     plugins: [
         laravel({
-<<<<<<< HEAD
             input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/BusquedaReserva/validar_fecha_busqueda.js'],
-=======
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/validar_fecha_busqueda.js'],
->>>>>>> 60cac1d1ed254d15451019046f924bcd1985a63c
             refresh: true,
         }),
     ],
